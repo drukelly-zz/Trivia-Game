f6ec9da831df4de3bcc88e8bc9630a73e21ca6ad
Dru Kelly <drukelly@gmail.com>
2019-03-28 15:36:59 -0700

    assets update + structure

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  
+  // timer
+  const countdownNumberEl = document.getElementById('countdown-number');
+  let countdown = 20;
+
+  countdownNumberEl.textContent = countdown;
+
+  setInterval(function() {
+    countdown = --countdown <= 0 ? 20 : countdown;
+
+    countdownNumberEl.textContent = countdown;
+  }, 1000);
 });
