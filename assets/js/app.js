d925d35eeb9acf3ae8a801a61f9d8ba3c8223c8f
Dru Kelly <drukelly@gmail.com>
2019-04-01 10:56:43 -0700

    better question/answers handling

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,42 +1,61 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let wrong,
-    right,
-    queryURL = "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple";
-  if (window.fetch) {
-    fetch(queryURL, {
-      method: "GET"
-    })
-      .then(result => result.json())
-      .then(response => {
-        // console.log(`fetch => ${response}`);
-        generateQuestion(response, 2);
-      });
-  } else {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", queryURL);
-    xhr.onload = event => {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          let response = JSON.parse(xhr.response);
-          // console.log(`xhr => ${response}`);
-          generateQuestion(response, 0);
-        } else {
-          console.error(xhr.responseText);
+  let index = 0,
+    messageDiv = document.querySelector(".message"),
+    qaDiv = document.querySelector(".question-and-answers"),
+    queryURL = "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple",
+    response,
+    right = 0,
+    selectedAnswer,
+    statsDiv = document.querySelector(".stats"),
+    wrong = 0;
+  const callApi = (response, index) => {
+    if (window.fetch) {
+      fetch(queryURL, {
+        method: "GET"
+      })
+        .then(result => result.json())
+        .then(response => {
+          // console.log(`fetch => ${response}`);
+          generateQuestion(response, index);
+        });
+    } else {
+      const xhr = new XMLHttpRequest();
+      xhr.open("GET", queryURL);
+      xhr.onload = event => {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            let response = JSON.parse(xhr.response);
+            // console.log(`xhr => ${response}`);
+            generateQuestion(response, index);
+          } else {
+            console.error(xhr.responseText);
+          }
         }
-      }
-    };
-    xhr.onerror = event => {
-      console.error(xhr.responseText);
-    };
-    xhr.send();
+      };
+      xhr.onerror = event => {
+        console.error(xhr.responseText);
+      };
+      xhr.send();
+    }
   }
-  // 
+  // Correct answer handling
   const isCorrect = (message) => {
-    console.log(message);
+    ++right;
+    ++index;
+    console.log(right);
+    qaDiv.classList.add("hide");
+    statsDiv.classList.add("hide");
+    messageDiv.innerHTML = `<h1>${message}</h1>`;
+    messageDiv.innerHTML += `<img src="https://media.giphy.com/media/w1XIBQlBjMTsc/giphy-downsized.gif" class="img-25" alt="Correct!">`;
   }
-  // 
+  // Wrong answer handling
   const isWrong = (message) => {
-    console.log(message);
+    ++wrong;
+    ++index;
+    qaDiv.classList.add("hide");
+    statsDiv.classList.add("hide");
+    messageDiv.innerHTML = `<img src="https://media.giphy.com/media/13JksdKh3B1LcQ/giphy-downsized.gif" class="img-25" alt="Wrong!">`;
+    messageDiv.innerHTML += `<h1>${message}</h1>`;
   }
   // Generate question template
   const generateQuestion = (response, index) => {
@@ -49,17 +68,17 @@ document.addEventListener("DOMContentLoaded", () => {
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat
     let answerButtons = shuffle(answers.flat());
     document.querySelector(".question").firstElementChild.innerHTML = question;
+    // Loop through each button
     answerButtons.forEach(answer => {
       let button = document.createElement("button");
-      button.setAttribute("data-option", answer);
       button.innerHTML = answer;
-      button.addEventListener("click", () => {
-        console.log(answer);
-        button.innerHTML === correctAnswer ? isCorrect("you're right!") : isWrong("you're wrong!");
+      button.addEventListener("click", (event) => {
+        selectedAnswer = button.innerHTML;
+        button.innerHTML === correctAnswer ? isCorrect("Correct!") : isWrong("Wrong!");
       });
       document.querySelector(".options").appendChild(button);
     });
-    console.log(`Answer is ${correctAnswer}`);
+    console.log(`The correct answer is ${correctAnswer}`);
   }
   // Shuffle Array
   // Based on the Fisher-Yates shuffle algorithm
@@ -79,18 +98,17 @@ document.addEventListener("DOMContentLoaded", () => {
   }
   const startGame = () => {
     let overlay = document.querySelector(".overlay"),
-      modal = document.querySelector(".modal"),
-      countdownDiv = document.getElementById("countdown"),
-      totalIndicatorDiv = document.querySelector(".total-indicator");
+      modal = document.querySelector(".modal");
     // Remove overlay and modal from the DOM
     overlay.remove();
     modal.remove();
     // Show divs by removing the "hide" class
-    countdownDiv.classList.remove("hide");
-    totalIndicatorDiv.classList.remove("hide");
+    statsDiv.classList.remove("hide");
+    qaDiv.classList.remove("hide");
     document.querySelector(".branding").classList.add("active");
     // Start the timer
     startTimer();
+    callApi(response, index);
   }
   // Start timer
   const startTimer = () => {
@@ -98,10 +116,15 @@ document.addEventListener("DOMContentLoaded", () => {
     const COUNTDOWN = 20;
     let countdown = 20;
     countdownNumber.textContent = countdown;
-    setInterval(function () {
+    setInterval(() => {
       countdown = --countdown <= 0 ? COUNTDOWN : countdown;
       countdownNumber.textContent = countdown;
-      if (countdown === 1) isWrong("you didn't answer!")
+      if (countdown === 1 && !selectedAnswer) isWrong("You didn't answer!")
     }, 1000);
   }
+  // Start the game with button
+  document.querySelector(".btn-start").addEventListener("click", (event) => {
+    event.preventDefault();
+    startGame();
+  });
 });
