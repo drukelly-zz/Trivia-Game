46c3934f7273f8abcf10be03b15116e145e83db1
Dru Kelly <drukelly@gmail.com>
2019-04-01 13:27:12 -0700

    cleanup

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let index = 0,
+  let
+    answers = [],
+    index = 0,
     messageDiv = document.querySelector(".message"),
     qaDiv = document.querySelector(".question-and-answers"),
     queryURL = "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple",
     response,
     right = 0,
-    selectedAnswer,
+    selectedAnswer = 0,
     statsDiv = document.querySelector(".stats"),
     wrong = 0;
-  const callApi = (response, index) => {
+  const callAPI = (response, index) => {
     if (window.fetch) {
       fetch(queryURL, {
         method: "GET"
@@ -45,8 +47,10 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log(right);
     qaDiv.classList.add("hide");
     statsDiv.classList.add("hide");
+    messageDiv.classList.remove("hide");
     messageDiv.innerHTML = `<h1>${message}</h1>`;
     messageDiv.innerHTML += `<img src="https://media.giphy.com/media/w1XIBQlBjMTsc/giphy-downsized.gif" class="img-25" alt="Correct!">`;
+    setTimeout(() => nextQuestion(response), 1500);
   }
   // Wrong answer handling
   const isWrong = (message) => {
@@ -54,15 +58,24 @@ document.addEventListener("DOMContentLoaded", () => {
     ++index;
     qaDiv.classList.add("hide");
     statsDiv.classList.add("hide");
+    messageDiv.classList.remove("hide");
     messageDiv.innerHTML = `<img src="https://media.giphy.com/media/13JksdKh3B1LcQ/giphy-downsized.gif" class="img-25" alt="Wrong!">`;
     messageDiv.innerHTML += `<h1>${message}</h1>`;
+    setTimeout(() => nextQuestion(response), 1500);
+  }
+  // Next question
+  const nextQuestion = (response) => {
+    callAPI(response, index);
+    messageDiv.classList.add("hide");
+    qaDiv.classList.remove("hide");
+    statsDiv.classList.remove("hide");
   }
   // Generate question template
   const generateQuestion = (response, index) => {
-    let answers = [],
-      question = response.results[index].question,
+    question = response.results[index].question,
       correctAnswer = response.results[index].correct_answer,
       incorrectAnswers = response.results[index].incorrect_answers;
+    answers = [];
     answers.push(correctAnswer);
     answers.push(incorrectAnswers);
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat
@@ -96,6 +109,7 @@ document.addEventListener("DOMContentLoaded", () => {
     Array.from(array);
     return array;
   }
+  // Starts the game
   const startGame = () => {
     let overlay = document.querySelector(".overlay"),
       modal = document.querySelector(".modal");
@@ -108,18 +122,21 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector(".branding").classList.add("active");
     // Start the timer
     startTimer();
-    callApi(response, index);
+    // Call the API
+    callAPI(response, index);
   }
-  // Start timer
+  // Start the timer
   const startTimer = () => {
     const countdownNumber = document.getElementById('countdown-number');
-    const COUNTDOWN = 20;
-    let countdown = 20;
+    // Change const COUNTDOWN and let countdown to desired number
+    // Note: Be sure to change (site.css, line 176) to match the animation duration
+    const COUNTDOWN = 10;
+    let countdown = 10;
     countdownNumber.textContent = countdown;
     setInterval(() => {
-      countdown = --countdown <= 0 ? COUNTDOWN : countdown;
+      countdown = --countdown < 0 ? COUNTDOWN : countdown;
       countdownNumber.textContent = countdown;
-      if (countdown === 1 && !selectedAnswer) isWrong("You didn't answer!")
+      if (countdown === 0 && selectedAnswer <= 0) isWrong("You didn't answer!")
     }, 1000);
   }
   // Start the game with button
